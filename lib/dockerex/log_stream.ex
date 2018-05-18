6343637d59d5b4d267f0363cfd37774194562b74
Michael Howell <michael@notriddle.com>
2018-05-18 21:00:59 +0000

    Get rid of the timeout in HTTPoison calls

--- lib/dockerex/log_stream.ex
+++ lib/dockerex/log_stream.ex
@@ -10,7 +10,7 @@ defmodule Dockerex.LogStream do
       %HTTPoison.AsyncHeaders{} ->
         continue(resp)
       after 60_000 ->
-        raise RuntimeError, "timed out: one minute wit no headers in LogStream"
+        raise RuntimeError, "timed out: one minute with no headers in LogStream"
     end
   end
 
@@ -20,8 +20,8 @@ defmodule Dockerex.LogStream do
         {[chunk], resp}
       %HTTPoison.AsyncEnd{} ->
         {:halt, resp}
-      after 3600_000 ->
-        raise RuntimeError, "timed out: one hour of inactivity in LogStream"
+      after 600_000 ->
+        raise RuntimeError, "timed out: ten minutes of inactivity in LogStream"
     end
   end
 
@@ -30,7 +30,7 @@ defmodule Dockerex.LogStream do
   end
 
   def stream(url, method, data, headers, options) do
-    options = [{:stream_to, self} | options]
+    options = [{:stream_to, self}, {:timeout, :infinity} | options]
     Stream.resource(fn -> start(url, method, data, headers, options) end, &continue/1, &finish/1)
   end
 
